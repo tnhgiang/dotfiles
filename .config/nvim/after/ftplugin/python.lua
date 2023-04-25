870f1cf22b71be9d543ee0af6275412975212fb9
tnhgiang <45427365+tnhgiang@users.noreply.github.com>
2023-04-25 18:52:30 +0700

    feat: Abbreviation for python files (#22)
    - Abbreviation for ipdb
    - Update description for autocmd

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -1,10 +1,18 @@
 local opt = vim.opt
 local g = vim.g
+local api = vim.api
+
 -- tabs & indentation
 opt.tabstop = 4 -- 4 spaces for tabs
 opt.shiftwidth = 4 -- 4 spaces for indent width
 -- Black convention
-g.pyindent_open_paren = 'shiftwidth()'
-g.pyindent_nested_paren = 'shiftwidth()'
-g.pyindent_continue = 'shiftwidth()'
-g.pyindent_close_paren = '-shiftwidth()'
+g.pyindent_open_paren = "shiftwidth()"
+g.pyindent_nested_paren = "shiftwidth()"
+g.pyindent_continue = "shiftwidth()"
+g.pyindent_close_paren = "-shiftwidth()"
+
+-- Abbreviations
+api.nvim_create_autocmd("BufEnter", {
+  desc = "Abbreviations for python files only",
+  command = [[iabbrev <buffer> db@ import ipdb; ipdb.set_trace # fmt: skip]],
+})
