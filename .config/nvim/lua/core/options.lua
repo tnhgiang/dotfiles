ceebde6728beaaeac208592579e897a216181eca
tnhgiang <giang.tnhoang@gmail.com>
2023-04-25 16:29:56 +0700

    perf: Set up mouse

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -39,3 +39,7 @@ opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
 opt.iskeyword:append("-") -- consider string-string as whole word
+
+-- set mouse
+opt.mouse = "a"
+opt.mousescroll = "ver:1,hor:1"
