39d9179b22e8f5f3cdc8900bab1358ec659a21f8
tnhgiang <giang.tnhoang@gmail.com>
2023-04-25 16:50:48 +0700

    Indentation

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -5,7 +5,7 @@ api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
 -- Highlight on yank
 api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank({ timeout = 250 })
-	end,
+  callback = function()
+    vim.highlight.on_yank({ timeout = 250 })
+  end,
 })
