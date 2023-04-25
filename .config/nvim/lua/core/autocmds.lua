f69eeb5ecd28a779677d6bd111da9db94bdd4fa0
tnhgiang <giang.tnhoang@gmail.com>
2023-04-25 16:48:14 +0700

    perf: Hightlight on yank and no comment on new line

---
+++ .config/nvim/lua/core/autocmds.lua
@@ -0,0 +1,11 @@
+local api = vim.api
+
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- Highlight on yank
+api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ timeout = 250 })
+	end,
+})
